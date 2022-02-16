3cbbff8667cf7d6c358667428cee164a20b746a1
weiZhen <likelovec@gmail.com>
2022-02-16 14:50:46 +0800

    🚀 添加热部署

---
+++ chat-serv/src/test/java/RunDev.java
@@ -0,0 +1,15 @@
+import chat.like.cn.ChatServ;
+import chat.like.cn.core.dev.Hotdev;
+import org.noear.solon.annotation.Import;
+
+/**
+ * @author <a href="mailto:likelovec@gmail.com">韦朕</a>
+ * @date 2022/2/16 14:00
+ */
+@Import(scanPackages = {"chat.like"})
+public class RunDev {
+
+    public static void main(String[] args) {
+        Hotdev.start(ChatServ.class, args);
+    }
+}
